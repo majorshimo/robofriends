ca064aea1d64f7118f5d3f8220210f1982788f5d
Simon <simonromano2007@gmail.com>
2018-10-15 08:36:57 -0700

    added redux and finalized project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
+
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
  
 class App extends Component {
-	constructor() {
-		super() 
-		//Important to know that if we will run a this.state function we need to run super()
-		this.state = {
-			robots: [],
-			//here robots is a state, whereas in the robots.js file it is a prop
-			searchfield: ''
-		}
-		//console.log('constructor');
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(response=> response.json())
-			.then(users => this.setState({robots: users}));
-			//console.log('componentDidMount');
-	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value })
+		this.props.onRequestRobots();
 	}
 
 	render(){
-		const { robots, searchfield } = this.state;
+		const { searchField, onSearchChange, robots, isPending } = this.props;
 		const filteredRobots = robots.filter(robot => {
-			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+			return robot.name.toLowerCase().includes(searchField.toLowerCase());
 		})
 		//console.log('render');
 		//the following if statement says that if the api cannot return anything or is taking too long
 		// it will return a screen that says loading
-		return !robots.length ? 
+		return isPending ? 
 			<h1> Loading</h1> :
 			(
 				<div className = 'tc'>
 					<h1 className ='f1'>RoboFriends</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
+					<SearchBox searchChange={onSearchChange}/>
 					<Scroll>
 						<ErrorBoundry>
 							<CardList robots ={filteredRobots}/>
@@ -52,4 +53,18 @@ class App extends Component {
 		}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
